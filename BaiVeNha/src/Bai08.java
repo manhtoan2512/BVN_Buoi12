722cfe8ee23f7176f8f1f07bc510633bc099e4f5
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-03-01 08:32:28 +0700

    Update Bai08.java

--- BaiVeNha/src/Bai08.java
+++ BaiVeNha/src/Bai08.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Bai08 {
 
     public static void main(String[] args) {
-        System.out.println("Nhập xâu kí tự: ");
+        System.out.print("Nhập xâu S1: ");
         String S1 = new Scanner(System.in).nextLine();
-        System.out.println("Nhập xâu tìm kiếm: ");
+        System.out.print("Nhập xâu S2: ");
         String S2 = new Scanner(System.in).nextLine();
         timKiem(S1,S2);
     }
